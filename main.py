5e2c9e058d53b06475e7d93955c6cf768f90a421
Muhammad Haris Khan <68476240+HARISKHAN-1729@users.noreply.github.com>
2024-05-30 14:32:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     training_dataset = torch.from_numpy(np.load('/kaggle/input/nerf-train-test/training_data.pkl', allow_pickle=True))
     testing_dataset = torch.from_numpy(np.load('/kaggle/input/nerf-train-test/testing_data.pkl', allow_pickle=True))
 
-    model = NerfModel(hidden_dim=32).to(device)
+    model = NerfModel(hidden_dim=256).to(device)
     model_optimizer = torch.optim.Adam(model.parameters(), lr=5e-4)
     scheduler = torch.optim.lr_scheduler.MultiStepLR(model_optimizer, milestones=[2, 4, 8], gamma=0.5)
     data_loader = DataLoader(training_dataset, batch_size=1024, shuffle=True)
